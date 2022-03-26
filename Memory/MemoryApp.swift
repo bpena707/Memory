e373977b5787dcb8e5026d25566cd648e53813bd
bpena707 <bpena.610@gmail.com>
2022-03-25 20:02:34 -0700

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Bryan Pena on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
